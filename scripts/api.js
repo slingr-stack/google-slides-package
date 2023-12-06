0972d1f0446318afea38fcd205d8d66b4f79d2b1
Angel Perez Personal <pasaperez@yahoo.com.ar>
2023-12-06 16:10:20 -0300

    creation of package

--- scripts/api.js
+++ scripts/api.js
@@ -24,12 +24,7 @@ function handleRequestWithRetry(requestFn, options, callbackData, callbacks) {
     try {
         return requestFn(options, callbackData, callbacks);
     } catch (error) {
-        sys.logs.info("[skeleton] Handling request..."+ JSON.stringify(error));
-        // TODO : If you use oauth uncomment this, otherwise delete this comment
-        /*
-        dependencies.oauth.functions.refreshToken('skeleton:refreshToken');
-        return requestFn(setAuthorization(options), callbackData, callbacks);
-        */
+        sys.logs.info("[googleslides] Handling request..."+ JSON.stringify(error));
     }
 }
 
@@ -41,28 +36,6 @@ function createWrapperFunction(requestFn) {
 
 for (let key in httpDependency) {
     if (typeof httpDependency[key] === 'function') httpService[key] = createWrapperFunction(httpDependency[key]);
-}
-
-// TODO If use oauth you will need the following two functions, otherwise delete them
-
-/**
- * Retrieves the access token.
- *
- * @return {void} The access token refreshed on the storage.
- */
-exports.getAccessToken = function () {
-    sys.logs.info("[skeleton] Getting access token from oauth");
-    return dependencies.oauth.functions.connectUser('skeleton:userConnected');
-}
-
-/**
- * Removes the access token from the oauth.
- *
- * @return {void} The access token removed on the storage.
- */
-exports.removeAccessToken = function () {
-    sys.logs.info("[skeleton] Removing access token from oauth");
-    return dependencies.oauth.functions.disconnectUser('skeleton:disconnectUser');
 }
 
 /****************************************************
@@ -80,7 +53,7 @@ exports.removeAccessToken = function () {
  */
 exports.get = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
-    return httpService.get(Skeleton(options), callbackData, callbacks);
+    return httpService.get(GoogleSlides(options), callbackData, callbacks);
 };
 
 /**
@@ -94,7 +67,7 @@ exports.get = function(path, httpOptions, callbackData, callbacks) {
  */
 exports.post = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
-    return httpService.post(Skeleton(options), callbackData, callbacks);
+    return httpService.post(GoogleSlides(options), callbackData, callbacks);
 };
 
 /**
@@ -108,7 +81,7 @@ exports.post = function(path, httpOptions, callbackData, callbacks) {
  */
 exports.put = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
-    return httpService.put(Skeleton(options), callbackData, callbacks);
+    return httpService.put(GoogleSlides(options), callbackData, callbacks);
 };
 
 /**
@@ -122,7 +95,7 @@ exports.put = function(path, httpOptions, callbackData, callbacks) {
  */
 exports.patch = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
-    return httpService.patch(Skeleton(options), callbackData, callbacks);
+    return httpService.patch(GoogleSlides(options), callbackData, callbacks);
 };
 
 /**
@@ -136,7 +109,7 @@ exports.patch = function(path, httpOptions, callbackData, callbacks) {
  */
 exports.delete = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
-    return httpService.delete(Skeleton(options), callbackData, callbacks);
+    return httpService.delete(GoogleSlides(options), callbackData, callbacks);
 };
 
 /**
@@ -150,7 +123,7 @@ exports.delete = function(path, httpOptions, callbackData, callbacks) {
  */
 exports.head = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
-    return httpService.head(Skeleton(options), callbackData, callbacks);
+    return httpService.head(GoogleSlides(options), callbackData, callbacks);
 };
 
 /**
@@ -164,7 +137,7 @@ exports.head = function(path, httpOptions, callbackData, callbacks) {
  */
 exports.options = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
-    return httpService.options(Skeleton(options), callbackData, callbacks);
+    return httpService.options(GoogleSlides(options), callbackData, callbacks);
 };
 
 exports.utils = {
@@ -201,10 +174,10 @@ exports.utils = {
      */
     getConfiguration: function (property) {
         if (!property) {
-            sys.logs.debug('[skeleton] Get configuration');
+            sys.logs.debug('[googleslides] Get configuration');
             return JSON.stringify(config.get());
         }
-        sys.logs.debug('[skeleton] Get property: '+property);
+        sys.logs.debug('[googleslides] Get property: '+property);
         return config.get(property);
     },
 
@@ -263,21 +236,18 @@ function isObject (obj) {
 let stringType = Function.prototype.call.bind(Object.prototype.toString)
 
 /****************************************************
+ Constants
+ ****************************************************/
+
+const GOOGLEWORKSPACE_API_AUTH_URL = "https://oauth2.googleapis.com/token";
+
+/****************************************************
  Configurator
  ****************************************************/
 
-// TODO This is for the uncommon case that you need to execute something when the app is redeployed or in the first call
-// TODO Remove this variable if you don't need it
-
-let init = true;
-
-// TODO Refactor the Skeleton function to your package name
-
-let Skeleton = function (options) {
-    if (init) { methodOnInit(); init= false; } // TODO Remove this line if you don't use the init variable
+let GoogleSlides = function (options) {
     options = options || {};
     options= setApiUri(options);
-    options= setAuthorization(options);
     options= setRequestHeaders(options);
     return options;
 }
@@ -287,54 +257,80 @@ let Skeleton = function (options) {
  ****************************************************/
 
 function setApiUri(options) {
+    let API_URL = config.get("GOOGLESLIDES_API_BASE_URL");
     let url = options.path || "";
     options.url = API_URL + url;
-    sys.logs.debug('[skeleton] Set url: ' + options.path + "->" + options.url);
+    sys.logs.debug('[googleslides] Set url: ' + options.path + "->" + options.url);
     return options;
 }
 
 function setRequestHeaders(options) {
     let headers = options.headers || {};
-    if (config.get("choice") === "apiKey") { // TODO: Set the authentication method, if needed or remove this if (Remove comments after set the url)
-        sys.logs.debug('[skeleton] Set header apikey');
-        headers = mergeJSON(headers, {"Authorization": "API-Key " + config.get("text")});
-    } 
+
+    sys.logs.debug('[googleslides] Set header Bearer');
     headers = mergeJSON(headers, {"Content-Type": "application/json"});
+    headers = mergeJSON(headers, {"Authorization": "Bearer "+getAccessTokenForAccount()});
+
+    if (headers.Accept === undefined || headers.Accept === null || headers.Accept === "") {
+        sys.logs.debug('[googleslides] Set header accept');
+        headers = mergeJSON(headers, {"Accept": "application/json"});
+    }
 
     options.headers = headers;
     return options;
 }
 
-function setAuthorization(options) { // TODO: Set the authorization method and verify prefix, if needed or remove this function (Remove comments after set the url)
-    sys.logs.debug('[skeleton] Setting header token oauth');
-    let authorization = options.authorization || {};
-    authorization = mergeJSON(authorization, {
-        type: "oauth2",
-        accessToken: sys.storage.get(config.get("oauth").id + ' - access_token', {decrypt:true}),
-        headerPrefix: "token"
-    });
-    options.authorization = authorization;
-    return options;
+function getAccessTokenForAccount(account) {
+    account = account || "account";
+    sys.logs.info('[googleslides] Getting access token for account: '+account);
+    let installationJson = sys.storage.get('installationInfo-GoogleSlides---'+account) || {id: null};
+    let token = installationJson.token || null;
+    let expiration = installationJson.expiration || 0;
+    if (!token || expiration < new Date().getTime()) {
+        sys.logs.info('[googleslides] Access token is expired or not found. Getting new token');
+        let res = httpService.post(
+            {
+                url: "https://oauth2.googleapis.com/token",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: {
+                    grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                    assertion: getJsonWebToken()
+                }
+            });
+        token = res.access_token;
+        let expires_at = res.expires_in;
+        expiration = new Date(new Date(expires_at) - 1 * 60 * 1000).getTime();
+        installationJson = mergeJSON(installationJson, {"token": token, "expiration": expiration});
+        sys.logs.info('[googleslides] Saving new token for account: ' + account);
+        sys.storage.replace('installationInfo-GoogleSlides---'+account, installationJson);
+    }
+    return token;
 }
 
-function methodOnInit(){
-    let refreshTokenResponse = httpService.post({
-        url: "https://example.com/",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded"
+function getJsonWebToken() {
+    let currentTime = new Date().getTime();
+    let futureTime = new Date(currentTime + ( 10 * 60 * 1000)).getTime();
+    let scopeProp= config.get("scope");
+    let scopes;
+    if (!!scopeProp) {
+        scopes = scopeProp.map(function (s) {
+            return "https://www.googleapis.com/auth/" + s;
+        });
+    }
+    let scopesGlobal = scopes.join(" ");
+    return sys.utils.crypto.jwt.generate(
+        {
+            iss: config.get("serviceAccountEmail"),
+            aud: GOOGLEWORKSPACE_API_AUTH_URL,
+            scope: scopesGlobal,
+            iat: currentTime,
+            exp: futureTime
         },
-        body: {"grant_type":"refresh_token","refresh_token" : config.get("refreshToken")},
-        authorization: {
-            type: "basic",
-            username: config.get("clientId"),
-            password: config.get("clientSecret")
-        }
-    });
-    sys.logs.debug('[skeleton] Refresh token response: ' + JSON.stringify(refreshTokenResponse));
-    // If you need to set a variable at application level, you can do it with _config.set (on redeploy its cleared)
-    _config.set("accessToken", refreshTokenResponse.access_token);
-    _config.set("refreshToken", refreshTokenResponse.refresh_token);
+        config.get("privateKey"),
+        "RS256"
+    )
 }
 
 function mergeJSON (json1, json2) {
@@ -347,21 +343,4 @@ function mergeJSON (json1, json2) {
         if(json2.hasOwnProperty(key)) result[key] = json2[key];
     }
     return result;
-}
-
-/****************************************************
- Extra helper
- ****************************************************/
-
-exports.callbackTest = function () {
-    log('test function arrived UI');
-    sys.ui.sendMessage({
-        scope: 'uiService:testUiService.testUiService',
-        name: 'callbackTest',
-        callbacks: {
-            callbackTest: function (originalMessage, callbackData) {
-                sys.logs.info('callbackTest');
-            }
-        }
-    });
 }
