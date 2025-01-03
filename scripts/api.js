d23d18f28b6b18065f946302e008cc01ff439c6f
germanm98 <germanmanzur98@gmail.com>
2025-01-03 00:06:13 -0300

    Added jwt authentication

--- scripts/api.js
+++ scripts/api.js
@@ -7,11 +7,6 @@ let httpReference = dependencies.http;
 let httpDependency = {
     get: httpReference.get,
     post: httpReference.post,
-    put: httpReference.put,
-    patch: httpReference.patch,
-    delete: httpReference.delete,
-    head: httpReference.head,
-    options: httpReference.options
 };
 
 let httpService = {};
@@ -25,8 +20,10 @@ function handleRequestWithRetry(requestFn, options, callbackData, callbacks) {
         return requestFn(options, callbackData, callbacks);
     } catch (error) {
         sys.logs.info("[googleslides] Handling request..."+ JSON.stringify(error));
-        dependencies.oauth.functions.refreshToken('googleslides:refreshToken');
-        return requestFn(setAuthorization(options), callbackData, callbacks);
+        if (config.get("authenticationMethod") == "oAuth2") {
+            dependencies.oauth.functions.refreshToken('googleslides:refreshToken');
+            return requestFn(setAuthorization(options), callbackData, callbacks);
+        }
     }
 }
 
@@ -47,7 +44,11 @@ for (let key in httpDependency) {
  */
 exports.getAccessToken = function () {
     sys.logs.info("[googleslides] Getting access token from oauth");
-    return dependencies.oauth.functions.connectUser('googleslides:userConnected');
+    if (authMethod === "oAuth2") {
+        return dependencies.oauth.functions.connectUser("googleslides:userConnected");
+    } else if (authMethod === "jwt") {
+        return getAccessTokenForAccount();
+    }
 }
 
 /**
@@ -56,8 +57,12 @@ exports.getAccessToken = function () {
  * @return {void} The access token removed on the storage.
  */
 exports.removeAccessToken = function () {
-    sys.logs.info("[googleslides] Removing access token from oauth");
-    return dependencies.oauth.functions.disconnectUser('googleslides:disconnectUser');
+    if (config.get("authenticationMethod") === "oAuth2") {
+        sys.logs.info("[googleslides] Removing access token from oauth");
+        return dependencies.oauth.functions.disconnectUser("googleslides:disconnectUser");
+    } else {
+        sys.logs.warn("[googleslides] JWT does not support token removal.");
+    }
 }
 
 /****************************************************
@@ -90,139 +95,6 @@ exports.get = function(path, httpOptions, callbackData, callbacks) {
 exports.post = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
     return httpService.post(GoogleSlides(options), callbackData, callbacks);
-};
-
-/**
- * Sends an HTTP PUT request to the specified URL with the provided HTTP options.
- *
- * @param {string} path         - The path to send the PUT request to.
- * @param {object} httpOptions  - The options to be included in the PUT request check http-service documentation.
- * @param {object} callbackData - Additional data to be passed to the callback functions. [optional]
- * @param {object} callbacks    - The callback functions to be called upon completion of the POST request. [optional]
- * @return {object}             - The response of the PUT request.
- */
-exports.put = function(path, httpOptions, callbackData, callbacks) {
-    let options = checkHttpOptions(path, httpOptions);
-    return httpService.put(GoogleSlides(options), callbackData, callbacks);
-};
-
-/**
- * Sends an HTTP PATCH request to the specified URL with the provided HTTP options.
- *
- * @param {string} path         - The path to send the PATCH request to.
- * @param {object} httpOptions  - The options to be included in the PATCH request check http-service documentation.
- * @param {object} callbackData - Additional data to be passed to the callback functions. [optional]
- * @param {object} callbacks    - The callback functions to be called upon completion of the POST request. [optional]
- * @return {object}             - The response of the PATCH request.
- */
-exports.patch = function(path, httpOptions, callbackData, callbacks) {
-    let options = checkHttpOptions(path, httpOptions);
-    return httpService.patch(GoogleSlides(options), callbackData, callbacks);
-};
-
-/**
- * Sends an HTTP DELETE request to the specified URL with the provided HTTP options.
- *
- * @param {string} path         - The path to send the DELETE request to.
- * @param {object} httpOptions  - The options to be included in the DELETE request check http-service documentation.
- * @param {object} callbackData - Additional data to be passed to the callback functions. [optional]
- * @param {object} callbacks    - The callback functions to be called upon completion of the DELETE request. [optional]
- * @return {object}             - The response of the DELETE request.
- */
-exports.delete = function(path, httpOptions, callbackData, callbacks) {
-    let options = checkHttpOptions(path, httpOptions);
-    return httpService.delete(GoogleSlides(options), callbackData, callbacks);
-};
-
-/**
- * Sends an HTTP HEAD request to the specified URL with the provided HTTP options.
- *
- * @param {string} path         - The path to send the HEAD request to.
- * @param {object} httpOptions  - The options to be included in the HEAD request check http-service documentation.
- * @param {object} callbackData - Additional data to be passed to the callback functions. [optional]
- * @param {object} callbacks    - The callback functions to be called upon completion of the HEAD request. [optional]
- * @return {object}             - The response of the HEAD request.
- */
-exports.head = function(path, httpOptions, callbackData, callbacks) {
-    let options = checkHttpOptions(path, httpOptions);
-    return httpService.head(GoogleSlides(options), callbackData, callbacks);
-};
-
-/**
- * Sends an HTTP OPTIONS request to the specified URL with the provided HTTP options.
- *
- * @param {string} path         - The path to send the OPTIONS request to.
- * @param {object} httpOptions  - The options to be included in the OPTIONS request check http-service documentation.
- * @param {object} callbackData - Additional data to be passed to the callback functions. [optional]
- * @param {object} callbacks    - The callback functions to be called upon completion of the OPTIONS request. [optional]
- * @return {object}             - The response of the OPTIONS request.
- */
-exports.options = function(path, httpOptions, callbackData, callbacks) {
-    let options = checkHttpOptions(path, httpOptions);
-    return httpService.options(GoogleSlides(options), callbackData, callbacks);
-};
-
-exports.utils = {
-
-    /**
-     * Converts a given date to a timestamp.
-     *
-     * @param {number | string} params      - The date to be converted.
-     * @return {object}                     - An object containing the timestamp.
-     */
-    fromDateToTimestamp: function(params) {
-        if (!!params) {
-            return {timestamp: new Date(params).getTime()};
-        }
-        return null;
-    },
-
-    /**
-     * Converts a timestamp to a date object.
-     *
-     * @param {number} timestamp            - The timestamp to convert.
-     * @return {object}                     - The date object representing the timestamp.
-     */
-    fromTimestampToDate: function(timestamp) {
-        return new Date(timestamp);
-    },
-
-    /**
-     * Gets a configuration from the properties.
-     *
-     * @param {string} property             - The name of the property to get.
-     *                                          If it is empty, return the entire configuration object.
-     * @return {string}                     - The value of the property or the whole object as string.
-     */
-    getConfiguration: function (property) {
-        if (!property) {
-            sys.logs.debug('[googleslides] Get configuration');
-            return JSON.stringify(config.get());
-        }
-        sys.logs.debug('[googleslides] Get property: '+property);
-        return config.get(property);
-    },
-
-    /**
-     * Concatenates a path with a param query and its value.
-     *
-     * @param path                          - The path to concatenate.
-     * @param key                           - The name of the param.
-     * @param value                         - The value of the param.
-     * @returns {string}                    - The concatenated path without coding parameters.
-     */
-    concatQuery: function (path, key, value) {
-        return path + ((!path || path.indexOf('?') < 0) ? '?' : '&') + key + "=" + value;
-    },
-
-    /**
-     * Merges two JSON objects into a single object.
-     *
-     * @param {Object} json1 - The first JSON object to be merged.
-     * @param {Object} json2 - The second JSON object to be merged.
-     * @return {Object} - The merged JSON object.
-     */
-    mergeJSON: mergeJSON,
 };
 
 /****************************************************
@@ -289,16 +161,79 @@ function setRequestHeaders(options) {
 }
 
 function setAuthorization(options) {
-    let authorization = options.authorization || {};
     sys.logs.debug('[googleslides] setting authorization');
-    authorization = mergeJSON(authorization, {
-        type: "oauth2",
-        accessToken: sys.storage.get(
-            'installationInfo-googleslides-User-'+sys.context.getCurrentUserRecord().id() + ' - access_token',{decrypt:true}),
-        headerPrefix: "Bearer"
-    });
-    options.authorization = authorization;
-    return options;
+    if (config.get("authenticationMethod") == "oAuth2") {
+        let authorization = options.authorization || {};
+        authorization = mergeJSON(authorization, {
+            type: "oauth2",
+            accessToken: sys.storage.get(
+                'installationInfo-googleslides-User-'+sys.context.getCurrentUserRecord().id() + ' - access_token',{decrypt:true}),
+            headerPrefix: "Bearer"
+        });
+        options.authorization = authorization;
+        return options;
+    } else {
+        options.headers = mergeJSON(options.headers, {"Authorization": "Bearer "+getAccessTokenForAccount()});
+
+    }
+}
+
+function getAccessTokenForAccount(account) {
+    account = account || "account";
+    sys.logs.info('[googleslides] Getting access token for account: '+account);
+    let installationJson = sys.storage.get('installationInfo-googleslides---'+account) || {id: null};
+    let token = installationJson.token || null;
+    let expiration = installationJson.expiration || 0;
+    if (!!token || expiration < new Date()) {
+        sys.logs.info('[googleslides] Access token is expired or not found. Getting new token');
+        let res = httpService.post(
+            {
+                url: "https://oauth2.googleapis.com/token",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: {
+                    grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                    assertion: getJsonWebToken()
+                }
+            });
+        token = res.access_token;
+        let expires_at = res.expires_in;
+        expiration = new Date(new Date(expires_at) - 1 * 60 * 1000).getTime();
+        installationJson = mergeJSON(installationJson, {"token": token, "expiration": expiration});
+        sys.logs.info('[googleslides] Saving new token for account: ' + account);
+        sys.storage.replace('installationInfo-googleslides---'+account, installationJson);
+    }
+    return token;
+}
+
+function getJsonWebToken() {
+    try{
+        let currentTime = new Date().getTime();
+        let futureTime = new Date(currentTime + ( 10 * 60 * 1000)).getTime();
+        let scopeProp= config.get("scope");
+        let scopes;
+        if (!!scopeProp) {
+            scopes = scopeProp.map(function (s) {
+                return "https://www.googleapis.com/auth/admin." + s;
+            });
+        }
+        let scopesGlobal = scopes.join(" ");
+        return sys.utils.crypto.jwt.generate(
+            {
+                iss: config.get("serviceAccountEmail"),
+                aud: config.get("GOOGLESLIDES_API_BASE_URL"),
+                scope: scopesGlobal,
+                iat: currentTime,
+                exp: futureTime
+            },
+            config.get("privateKey"),
+            "RS256"
+        );
+    } catch (error) {
+        sys.logs.error("[googleslides] Error generating JWT: " + JSON.stringify(error));
+        throw new Error("[googleslides] Failed to generate JWT.");
+    }
 }
 
 function mergeJSON (json1, json2) {
