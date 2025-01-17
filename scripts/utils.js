b94979cea12161e064ff8e80ab9d9d234235a2f1
pasaperez-slingr <119679724+pasaperez-slingr@users.noreply.github.com>
2025-01-17 17:38:01 -0300

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -14,13 +14,3 @@ exports.getConfiguration = function (property) {
     }
     return config.get(property);
 };
-
-/**
- * Verifies the token is the same as the verification token.
- *
- * @param {string} token                - The token to verify
- * @return {boolean}                    - True if the signature is valid, false otherwise.
- */
-exports.verifyToken = function (token) {
-    return token === config.get("verificationToken");
-};
